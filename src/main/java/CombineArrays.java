02cd691c85728436ee6fffa690a26f3a7b228bac
mohammed5000 <mohammed5000@github.com>
2026-02-19 19:58:11 +0000

    Test run: 0/2 passed, 2 failed, 0 skipped - 2026-02-19T19:58:11.750Z

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,10 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-       int [] combineArray = new int [arr1.length + arr2.length];
+        int[] combineArray = new int [arr1.length + arr2.length];
+       for (int i = 0; i < arr1.length; ++i) {
+        combineArray[i] = arr1[i] + arr2[i];
+       }
         return combineArray;
     }
 }
